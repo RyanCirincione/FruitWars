a2a1f1d8c0c2dbf58fa217b448bd7c8fa2618c06
Ryan Goldstein <ryanisaacg@gmail.com>
2017-03-28 21:06:18 -0400

    Created a one-button menu before the game

--- src/fruitwars/FruitWars.java
+++ src/fruitwars/FruitWars.java
@@ -1,15 +1,10 @@
 package fruitwars;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class FruitWars extends Application
 {
-	final static int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
 
 	public static void main(String[] args)
 	{
@@ -19,30 +14,7 @@ public class FruitWars extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-		Game game = Game.construct(WINDOW_WIDTH, WINDOW_HEIGHT);
-		stage.setScene(game);
-		
-		stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-			game.g.getCanvas().setWidth(newVal.doubleValue());
-		});
-		stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-			game.g.getCanvas().setHeight(newVal.doubleValue());
-		});
-
-		new AnimationTimer()
-		{
-			long previousMilli = 0;
-
-			public void handle(long currentNano)
-			{
-				long currentMilli = currentNano / 1000000;
-				long delta = currentMilli - previousMilli;
-				if (previousMilli != 0)
-					game.tick(delta);
-				game.draw(delta);
-				previousMilli = currentMilli;
-			}
-		}.start();
+		stage.setScene(Menu.construct(stage));
 		stage.show();
 	}
 }
---
+++ src/fruitwars/Menu.java
@@ -0,0 +1,56 @@
+package fruitwars;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Menu extends Scene
+{
+	final static int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+	
+	public static Menu construct(Stage stage) 
+	{
+		Group root = new Group();
+		
+		Button button = new Button();
+		button.setText("Play game");
+		button.setOnMouseClicked(evt -> {
+			Game game = Game.construct(WINDOW_WIDTH, WINDOW_HEIGHT);
+			stage.setScene(game);
+			
+			stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+				game.g.getCanvas().setWidth(newVal.doubleValue());
+			});
+			stage.heightProperty().addListener((obs, oldVal, newVal) -> {
+				game.g.getCanvas().setHeight(newVal.doubleValue());
+			});
+
+			new AnimationTimer()
+			{
+				long previousMilli = 0;
+
+				public void handle(long currentNano)
+				{
+					long currentMilli = currentNano / 1000000;
+					long delta = currentMilli - previousMilli;
+					if (previousMilli != 0)
+						game.tick(delta);
+					game.draw(delta);
+					previousMilli = currentMilli;
+				}
+			}.start();
+		});
+		
+		root.getChildren().add(button);
+		
+		return new Menu(root);
+	}
+
+	public Menu(Group root)
+	{
+		super(root);
+	}
+
+}
