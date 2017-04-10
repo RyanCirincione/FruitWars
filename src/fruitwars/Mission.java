10b7498ae981f9677ce70b5f86b5586b309d9779
Ryan Goldstein <ryanisaacg@gmail.com>
2017-04-09 21:17:34 -0400

    Registered the class names of all units to their constructors

--- src/fruitwars/Mission.java
+++ src/fruitwars/Mission.java
@@ -1,22 +1,41 @@
 package fruitwars;
 
-import java.awt.geom.Point2D;
 import java.util.HashMap;
 
-import data.EntityStore;
 import entity.Blueberry;
-import entity.Entity;
+import entity.BlueberryBush;
+import entity.Cauliflower;
+import entity.Grape;
+import entity.GrapeVine;
+import entity.Potato;
+import entity.Strawberry;
+import entity.StrawberryBush;
+import entity.StructureBuilder;
+import entity.UnitBuilder;
 
 public class Mission
 {
+	private static HashMap<String, StructureBuilder> structure;
+	private static HashMap<String, UnitBuilder> units;
 	
-	private static HashMap<String, EntityBuilder> constructors;
 	
-	private static void register(Class<?> cls, EntityBuilder ctor) {
-		constructors.put(cls.getName(), ctor);
+	private static void register(Class<?> cls, UnitBuilder ctor) {
+		units.put(cls.getName(), ctor);
+	}
+	
+	private static void register(Class<?> cls, StructureBuilder ctor) {
+		structure.put(cls.getName(), ctor);
 	}
 	
 	static {
 		register(Blueberry.class, Blueberry::new);
+		register(BlueberryBush.class, BlueberryBush::new);
+		register(Grape.class, Grape::new);
+		register(GrapeVine.class, GrapeVine::new);
+		register(Strawberry.class, Strawberry::new);
+		register(StrawberryBush.class, StrawberryBush::new);
+		register(Cauliflower.class, Cauliflower::new);
+		register(Potato.class, Potato::new);
+
 	}
 }
